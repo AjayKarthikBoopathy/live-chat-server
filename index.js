5aa898121b61517bddac53e83150abc7d3545ab1
AjayKarthikBoopathy <ajaykarthikbak97@gmail.com>
2024-08-08 14:36:05 +0530

    edit

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log("Server is Running..."));
+const server = app.listen(PORT, console.log("Server is Running..."));
 
 // Web Socket
 const io = require("socket.io")(server, {
